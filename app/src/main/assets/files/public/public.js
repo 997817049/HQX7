34e4ea8f65c7665b75a9e383b1cdac56904bfca1
GitHub <997817049@qq.com>
2020-05-09 18:40:21 +0800

    修改ip

--- app/src/main/assets/files/public/public.js
+++ app/src/main/assets/files/public/public.js
@@ -1,5 +1,5 @@
-serverIp = "http://192.168.0.102:8888/";
-// serverIp = "http://49.4.114.114:8888/";
+// serverIp = "http://192.168.0.102:8888/";
+serverIp = "http://49.4.114.114:8888/";
 screen_h = $(window).height();
 screen_w = $(window).width();
 
